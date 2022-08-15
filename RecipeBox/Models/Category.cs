4571684f7e90ae8c3c5cfe3272f4686dec0db27b
Sue Roberts <suerobertstx@yahoo.com>
2022-08-15 11:24:53 -0500

    Add initial Program

---
+++ RecipeBox/Models/Category.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeBox.Models
+{
+  public class Category
+  {
+    public Category()
+    {
+      this.JoinEntities = new HashSet<CategoryRecipe>();
+    }
+    public string Name { get; set; }
+    public int CategoryId { get; set; }
+
+    public virtual ICollection<CategoryRecipe> JoinEntities { get; set; }    
+  }
+}
